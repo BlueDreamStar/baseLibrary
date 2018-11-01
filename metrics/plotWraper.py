6185e836f62fd0c485511383c5ad3ac620fcdfbf
BlueDreamStar <wpf2106@gmail.com>
2018-11-01 16:27:44 +0800

    Modified the plotWraper

--- metrics/plotWraper.py
+++ metrics/plotWraper.py
@@ -5,7 +5,7 @@
 # @Desc:   Welcome to my world!
 # @Motto:  Brave & Naive!
 # @Last Modified by:   BlueDreamStar
-# @Last Modified time: 2018-10-30 18:07:03
+# @Last Modified time: 2018-11-01 11:31:30
 import numpy as np
 from matplotlib.ticker import MultipleLocator, FormatStrFormatter  
 from pylab import *
@@ -19,7 +19,7 @@ font = {'family' : 'serif',
         }
 
 ## wraper the plot function
-def plotAndSaveFigure(iters, data, colors, labels, axis_label, outSaveFile, lengendLoc="upper right", lwValue=1.6, multiNum=2, xlimit=None, ylimit=None):
+def plotAndSaveFigure(iters, data, colors, labels, axis_label, outSaveFile, lengendLoc="upper right", lwValue=1.6, multiNum=2, xlimit=None, ylimit=None, xtick=None, ytick=None):
 	if len(colors) <= 0: return
 # 	figure(1)
 	foo_fig = plt.gcf() # get current figure
@@ -34,6 +34,9 @@ def plotAndSaveFigure(iters, data, colors, labels, axis_label, outSaveFile, leng
 	ax.set_ylabel(axis_label[1],fontdict=font)
 	if not xlimit == None: plt.xlim((xlimit[0],xlimit[1]))
 	if not ylimit == None: plt.ylim((ylimit[0],ylimit[1]))
+	if not xtick == None: plt.xticks(iters, xtick)
+	if not xtick == None: ax.set_xticklabels(xtick,rotation=90)
+	if not ytick == None: plt.yticks(iters, ytick)
 	xmajorLocator = MultipleLocator(multiNum)
 	plt.legend(loc=lengendLoc, prop={'size': 16}) 
 	foo_fig.savefig('%s.pdf' %(outSaveFile), bbox_inches='tight', format='pdf', dpi=1000)
